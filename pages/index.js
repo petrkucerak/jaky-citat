02a8ee278599113f11879efae907f62ab8c6f35d
Petr Kučera <petr@khome.cz>
2021-12-23 11:33:49 +0000

    Add PWA suppoart elements

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import { useState } from "react";
-import {data} from "/pages/data/data.js"
+import { data } from "/pages/data/data.js";
 
 function randomIntFromInterval(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
@@ -10,13 +10,60 @@ function getText() {
   return data[randomIntFromInterval(0, data.length - 1)];
 }
 
+const name = "Co dneska vařit?";
+const description = "Netušíš, co dneska uvařit? Nech si poradit!";
+const theme_color = "#fbbf24";
+const url = "url";
+
 export default function Home() {
   const [text, setText] = useState("Fakt netušíš?");
   return (
     <div className="w-full dark:bg-black">
       <Head>
-        <title>Co dneska vařit?</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>{name}</title>
+        <meta name="application-name" content={name} />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="PWA App" />
+        <meta name="description" content={description} />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="msapplication-tap-highlight" content="no" />
+        <meta name="theme-color" content={theme_color} />
+
+        <link rel="apple-touch-icon" href="/favicon/apple-touch-icon.png" />
+
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/manifest.json" />
+
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:url" content={url} />
+        <meta name="twitter:title" content={name} />
+        <meta name="twitter:description" content={description} />
+        <meta name="twitter:site" content="@petrkucerak" />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content={name} />
+        <meta property="og:description" content={description} />
+        <meta property="og:site_name" content={name} />
+        <meta property="og:url" content={url} />
+
+        <link rel="shortcut icon" href="/favicon/favicon.ico" />
+        <meta name="msapplication-TileColor" content="#000000" />
+        <meta name="theme-color" content={theme_color} />
+
+        <meta name="description" content={description} />
       </Head>
 
       <main className="flex flex-col items-center min-h-screen justify-center">
@@ -29,7 +76,9 @@ export default function Home() {
         >
           poraď mi
         </button>
-        <div className="text-lg mt-5 text-center dark:text-white mx-5">{text}</div>
+        <div className="text-lg mt-5 text-center dark:text-white mx-5">
+          {text}
+        </div>
       </main>
     </div>
   );
