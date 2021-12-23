4183e1aaa4516ce36d9eb56f45bb21a52313cb6f
Petr Kučera <petr@khome.cz>
2021-12-23 10:39:37 +0000

    Dynamic render content.

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,20 @@
 import Head from "next/head";
+import { useState } from "react";
+
+const data = ["brambory", "hrusky", "nudle"];
+let text = "Tak co to dneska bude?";
+
+function randomIntFromInterval(min, max) {
+  // min and max included
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function getText() {
+  return data[randomIntFromInterval(0, data.length - 1)];
+}
 
 export default function Home() {
+  const [text, setText] = useState("Co to denska bude?");
   return (
     <div className="w-full">
       <Head>
@@ -9,8 +23,16 @@ export default function Home() {
       </Head>
 
       <main className="flex flex-col items-center min-h-screen justify-center">
-        <h1 className="text-4xl font-bold mb-8 text-center">Co dneska vařit?</h1>
-        <button className="p-4 bg-emerald-600 rounded-full drop-shadow-xl text-emerald-50 font-semibold uppercase text-xl">poraď mi</button>
+        <h1 className="text-4xl font-bold mb-8 text-center">
+          Co dneska vařit?
+        </h1>
+        <button
+          onClick={() => setText(getText())}
+          className="p-4 bg-emerald-600 rounded-full drop-shadow-xl text-emerald-50 font-semibold uppercase text-xl"
+        >
+          poraď mi
+        </button>
+        <div>{text}</div>
       </main>
     </div>
   );
