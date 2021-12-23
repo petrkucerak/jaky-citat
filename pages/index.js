81ab82b29abb441183c65f3db8801c8782281e57
Petr Kučera <petr@khome.cz>
2021-12-23 10:20:40 +0000

    Add basic style

--- pages/index.js
+++ pages/index.js
@@ -2,15 +2,15 @@ import Head from "next/head";
 
 export default function Home() {
   return (
-    <div className="flex">
+    <div className="w-full">
       <Head>
         <title>Co dneska vařit?</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="">
-        <h1>Co dneska vařit?</h1>
-        <div className="">poraď mi</div>
+      <main className="flex flex-col items-center min-h-screen justify-center">
+        <h1 className="text-4xl font-bold mb-8 text-center">Co dneska vařit?</h1>
+        <button className="p-4 bg-emerald-600 rounded-full drop-shadow-xl text-emerald-50 font-semibold uppercase text-xl">poraď mi</button>
       </main>
     </div>
   );
